067be3438d3f15f079b34934a765b1f4713718b9
priya298 <priyachowdary298@gmail.com>
2018-05-18 11:28:00 +0530

    added one program

---
+++ NewWeekEnd2/src/test/java/com/hdfc/load/eduloan/NewWeekEnd2/LinkTesting1.java
@@ -0,0 +1,31 @@
+package com.hdfc.load.eduloan.NewWeekEnd2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.AfterClass;
+
+public class LinkTesting1 extends BasePage
+{
+	
+  
+  @BeforeClass
+  @Parameters
+  public void StartProcess(String browser) throws Exception 
+  {
+	 browserLaunch(browser, getData("eenaduurl"));
+  }
+
+  @Test
+  public void SingleLink()
+  {
+	  
+  }
+  
+  @AfterClass
+  public void EndProcess() 
+  {
+	  
+  }
+
+}
