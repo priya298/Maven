476ec62a6885153d446aeabe3b3ee997a89a86eb
priya298 <priyachowdary298@gmail.com>
2018-05-18 11:52:31 +0530

    added one method

--- NewWeekEnd2/src/test/java/com/hdfc/load/eduloan/NewWeekEnd2/LinkTesting1.java
+++ NewWeekEnd2/src/test/java/com/hdfc/load/eduloan/NewWeekEnd2/LinkTesting1.java
@@ -3,6 +3,7 @@ package com.hdfc.load.eduloan.NewWeekEnd2;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterClass;
 
 public class LinkTesting1 extends BasePage
@@ -13,13 +14,13 @@ public class LinkTesting1 extends BasePage
   @Parameters
   public void StartProcess(String browser) throws Exception 
   {
-	 browserLaunch(browser, getData("flipkartUrl"));
+	 browserLaunch(browser, getData("eenaduurl"));
   }
 
   @Test
   public void SingleLink()
   {
-	  
+	  driver.findElement(By.linkText("Register Now")).click();
   }
   
   @AfterClass
